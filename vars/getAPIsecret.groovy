e1d0267a6685da8f59cd5c88fe90bbcca4005990
RatanBekal <ratankb@outlook.com>
2021-07-04 15:00:12 +0530

    Update getAPIsecret.groovy

--- vars/getAPIsecret.groovy
+++ vars/getAPIsecret.groovy
@@ -5,9 +5,9 @@ def call(Map params){
   echo params.secret
   def response= httppostcall token:params.apitoken,requestBody:params.body,url:params.url,secretKey:params.secret
   println response
-  echo "call base64 func with value" response.message
+  echo "call base64 func with value" 
   env.SECRET_NAME=base64decode str:response.message
-  echo "call base64 func with value" response.id
+  echo "call base64 func with value" 
   env.SECRET_PASSWORD=base64decode str:response.id.toString() 
   echo "from with in getAPIsed func"
   echo env.SECRET_NAME
