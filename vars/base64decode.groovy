fd3ed5fcc563e18438fec3077c79a286f5b093bc
RatanBekal <ratankb@outlook.com>
2021-07-04 15:01:31 +0530

    Update base64decode.groovy

--- vars/base64decode.groovy
+++ vars/base64decode.groovy
@@ -3,6 +3,7 @@ def call(Map params){
   echo params.str
   def s= params.str
   byte[] decoded =s.decodeBase64()
-  echo "decoded value" s.decodeBase64()
+  echo "decoded value" 
+  println s.decodeBase64()
   return new String(decoded)
 }
