9abba610c0bcf6f7f12eea5561baf5b8265a57cf
RatanBekal <ratankb@outlook.com>
2021-07-05 20:57:51 +0530

    Update getAPIsecret.groovy

--- vars/getAPIsecret.groovy
+++ vars/getAPIsecret.groovy
@@ -4,6 +4,7 @@ def call(Map params){
     def response= httppostcall token:params.apitoken,requestBody:params.body,url:params.url,secret:params.secret
     //env.SECRET_NAME= response.message
     //env.SECRET_PASSWORD= response.id.toString() 
+    echo "~~~~~~~~~ print response ~~~~~~~"
     println(response.message)
     println(response.success)
     println(response.data)
