a2ed2057c6ae58b37fd47e3b4a72ab21a493f858
RatanBekal <ratankb@outlook.com>
2021-07-05 20:48:37 +0530

    Update httpgetcall.groovy

--- old_bkp/httpgetcall.groovy
+++ old_bkp/httpgetcall.groovy
@@ -1,14 +1,14 @@
 import groovy.json.JsonSlurper
 def call(Map params){
-  echo "**********************  Inside httppostcall func*******"
+  echo "******************  Inside http GET call func*******"
 
   def response= httpRequest acceptType: 'APPLICATION_JSON', 
                             contentType: 'APPLICATION_JSON',
                             //authentication: params.secret,
-                            customHeaders: [[name: 'authtoken', value:params.token]], 
-                            httpMode: 'POST',
+                            //customHeaders: [[name: 'name', value: params.secret]], 
+                            httpMode: 'GET',
                             ignoreSslErrors: true, 
-                            requestBody: "{\"username\":\""+params.secret+"\"}",
+                            requestBody: params.requestBody, 
                             responseHandle: 'NONE', 
                             url: params.url, 
                             wrapAsMultipart: false
