51b7d9b706877b2260f0f2f9dca5c2265b4008a9
RatanBekal <ratankb@outlook.com>
2021-07-05 12:18:10 +0530

    Update httppostcall.groovy

--- vars/httppostcall.groovy
+++ vars/httppostcall.groovy
@@ -1,13 +1,11 @@
 import groovy.json.JsonSlurper
 def call(Map params){
-  echo "************************************************  Inside httppostcall func*******"
-  echo params.token
-  echo params.url
-  echo params.secret
+  echo "**********************  Inside httppostcall func*******"
+
   def response= httpRequest acceptType: 'APPLICATION_JSON', 
                             contentType: 'APPLICATION_JSON',
                             //authentication: params.secret,
-                            customHeaders: [[maskValue: true, name: 'x-api-key', value: params.token]], 
+                            customHeaders: [[name: 'x-api-key', value: params.token]], 
                             httpMode: 'POST',
                             ignoreSslErrors: true, 
                             requestBody: params.requestBody, 
