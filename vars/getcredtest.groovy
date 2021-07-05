97baef0ba0e3fdd4e20f9f59bcdb85afcc5544b1
RatanBekal <ratankb@outlook.com>
2021-07-05 20:37:23 +0530

    Delete getcredtest.groovy

--- vars/getcredtest.groovy
+++
@@ -1,5 +0,0 @@
-def call()
-{
- getAPIsecret apitoken:'DEMO-APIKEY',sectet:"github",url:"https://api.thecatapi.com/v1/votes",body:"{\"image_id\":\"asf\",\"sub_id\":\"my-user-1234\",\"value\":1}"
-  
-}
