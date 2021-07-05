53505146db91d0b5d34801f8f5e060dd03bdecc8
RatanBekal <ratankb@outlook.com>
2021-07-05 20:49:13 +0530

    Create httppostcall.groovy

---
+++ old_bkp/httppostcall.groovy
@@ -0,0 +1,18 @@
+import groovy.json.JsonSlurper
+def call(Map params){
+  echo "**********************  Inside httppostcall func*******"
+
+  def response= httpRequest acceptType: 'APPLICATION_JSON', 
+                            contentType: 'APPLICATION_JSON',
+                            //authentication: params.secret,
+                            customHeaders: [[name: 'authtoken', value:params.token]], 
+                            httpMode: 'POST',
+                            ignoreSslErrors: true, 
+                            requestBody: "{\"username\":\""+params.secret+"\"}",
+                            responseHandle: 'NONE', 
+                            url: params.url, 
+                            wrapAsMultipart: false
+  println response.content
+  return new JsonSlurper().parseText(response.content)
+  
+}
