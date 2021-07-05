de070206da93a0d6d01c3a7382e197d7d0167226
RatanBekal <ratankb@outlook.com>
2021-07-05 22:11:17 +0530

    Update base64decode.groovy

--- vars/base64decode.groovy
+++ vars/base64decode.groovy
@@ -1,8 +1,7 @@
 def call(Map request){
-  echo "*************inside base-decode****************"
+  
   def masksec= request.str
   byte[] decoded = masksec.decodeBase64()
-  println masksec.decodeBase64()
-  println decoded
+
   return new String(decoded)
 }
